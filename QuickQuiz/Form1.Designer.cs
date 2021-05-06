a77b59253aa476fee719ab94028415028ff755bc
N1ikHard <N1ik@yandex.ru>
2021-05-06 11:30:12 +0300

    Добавьте файлы проекта.

---
+++ QuickQuiz/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace QuickQuiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label_result = new System.Windows.Forms.Label();
+            this.button_last = new System.Windows.Forms.Button();
+            this.radioButton_answer3 = new System.Windows.Forms.RadioButton();
+            this.radioButton_answer2 = new System.Windows.Forms.RadioButton();
+            this.label_question1 = new System.Windows.Forms.Label();
+            this.radioButton_answer1 = new System.Windows.Forms.RadioButton();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label_result, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.button_last, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.radioButton_answer3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.radioButton_answer2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label_question1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.radioButton_answer1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28531F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28531F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28531F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28531F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28531F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28531F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28816F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(619, 571);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label_result
+            // 
+            this.label_result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_result.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_result.Location = new System.Drawing.Point(3, 486);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(613, 85);
+            this.label_result.TabIndex = 6;
+            this.label_result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button_last
+            // 
+            this.button_last.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_last.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_last.Location = new System.Drawing.Point(30, 420);
+            this.button_last.Margin = new System.Windows.Forms.Padding(30, 15, 30, 0);
+            this.button_last.Name = "button_last";
+            this.button_last.Size = new System.Drawing.Size(559, 66);
+            this.button_last.TabIndex = 5;
+            this.button_last.Text = "Previous";
+            this.button_last.UseVisualStyleBackColor = true;
+            this.button_last.Click += new System.EventHandler(this.button_last_Click);
+            // 
+            // radioButton_answer3
+            // 
+            this.radioButton_answer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radioButton_answer3.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton_answer3.Location = new System.Drawing.Point(3, 246);
+            this.radioButton_answer3.Name = "radioButton_answer3";
+            this.radioButton_answer3.Size = new System.Drawing.Size(613, 75);
+            this.radioButton_answer3.TabIndex = 3;
+            this.radioButton_answer3.TabStop = true;
+            this.radioButton_answer3.Tag = "2";
+            this.radioButton_answer3.Text = "Ответ3";
+            this.radioButton_answer3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton_answer3.UseVisualStyleBackColor = true;
+            this.radioButton_answer3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton_answer1_MouseClick);
+            // 
+            // radioButton_answer2
+            // 
+            this.radioButton_answer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radioButton_answer2.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton_answer2.Location = new System.Drawing.Point(3, 165);
+            this.radioButton_answer2.Name = "radioButton_answer2";
+            this.radioButton_answer2.Size = new System.Drawing.Size(613, 75);
+            this.radioButton_answer2.TabIndex = 2;
+            this.radioButton_answer2.TabStop = true;
+            this.radioButton_answer2.Tag = "1";
+            this.radioButton_answer2.Text = "Ответ2";
+            this.radioButton_answer2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton_answer2.UseVisualStyleBackColor = true;
+            this.radioButton_answer2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton_answer1_MouseClick);
+            // 
+            // label_question1
+            // 
+            this.label_question1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_question1.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_question1.Location = new System.Drawing.Point(3, 0);
+            this.label_question1.Name = "label_question1";
+            this.label_question1.Size = new System.Drawing.Size(613, 81);
+            this.label_question1.TabIndex = 0;
+            this.label_question1.Text = "Вопрос";
+            this.label_question1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // radioButton_answer1
+            // 
+            this.radioButton_answer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radioButton_answer1.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton_answer1.Location = new System.Drawing.Point(3, 84);
+            this.radioButton_answer1.Name = "radioButton_answer1";
+            this.radioButton_answer1.Size = new System.Drawing.Size(613, 75);
+            this.radioButton_answer1.TabIndex = 1;
+            this.radioButton_answer1.TabStop = true;
+            this.radioButton_answer1.Tag = "0";
+            this.radioButton_answer1.Text = "Ответ1";
+            this.radioButton_answer1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton_answer1.UseVisualStyleBackColor = true;
+            this.radioButton_answer1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton_answer1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 571);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Тест";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button_last;
+        private System.Windows.Forms.RadioButton radioButton_answer3;
+        private System.Windows.Forms.RadioButton radioButton_answer2;
+        private System.Windows.Forms.Label label_question1;
+        private System.Windows.Forms.RadioButton radioButton_answer1;
+        private System.Windows.Forms.Label label_result;
+    }
+}
+
---
+++ QuickQuiz/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.IO;
+using System.Text;                  
+using System.Windows.Forms;         
+//Событие по изменению состояния радиобутон не работает , если в двух разных
+//Стоящих друг за другом вопросах правильные ответы находятся на одних и тех
+//же позициях
+//Нажатие на радиобаттон- переход к новому вопросу
+//Сохранение выбора пользователя , при переходе на прошлый вопрос
+//Рандомная расстановка вариантов ответов
+namespace QuickQuiz
+{
+
+    public partial class Form1 : Form
+    {
+        string[] Questions;                     //Массив , содержащий вопросы
+        string[,] Answers;                      //Массив , содержащий ответы
+        string quiz_file_path = "Quiz.txt";     //Название файла , содержайщего вопросы и ответы
+        bool[,] radioButtonsState;              //Массив , содержащий состояния radiobutton
+        string[,] ArrayWithMixedAnswer;         //Массив , сохраняющий состояния radiobutton.text
+        bool flag;                              //Флаг , для повторного прохождения теста
+        int total;                              //Поля для размерности массивов
+        int current_nr;                         //Счетчик 
+        int score;                              //Очки 
+        int [] correct_number;                  //Массив , содержащий правильные ответы
+        Random rnd = new Random();              //Рандомайзер для перемешивания вопросов
+        
+        public Form1()
+        {
+            InitializeComponent();
+            LoadQuiz();                         //Загрузка строк из файла
+        }
+        private void LoadQuiz()                                    //Метод , для загрузки вопросов из файла с дальнейшим
+        {                                                          //Распределением их по разным массивам
+            try
+            {
+                string[] lines = File.ReadAllLines(quiz_file_path,Encoding.UTF8);   //Загрузка текста из файла
+                total = lines.Length / 4;              //мы знаем , что  на 1 вопрос с 3 ответами приходится 4 строки
+                Answers = new string[total, 3];        //=> кол-во строк вопросов равно строки поделить на 4       
+                Questions = new string[total];         //В дальнейшем в двумерный массив с ответами выделяем место
+                correct_number = new int[total];       //где под 1 вопрос 3 ответа , затем  выделяем память , для массива
+                ArrayWithMixedAnswer = new string[total, 3];  //который будет содержать в себе индексы правильных ответов
+                radioButtonsState = new bool[total, 3];  //Задаем размер массиву 
+                score = 0;                             //Первоначально количество очков=0
+                flag = true;                           //Переключаем флаг
+                current_nr = 0;                        //Счетчик в положение 0
+                for (int i = 0; i < total; i++)        //Заполнение массива 
+                {                                                   
+                    Questions[i] = lines[i * 4];       //Заполняем массив строками с вопросами
+                    for (int j = 0; j < Answers.GetLength(1); j++)
+                    {
+                        Answers[i, j] = lines[i * 4 + j + 1];   //Этот массив заполняем строками с ответами
+                        ArrayWithMixedAnswer[i, j] = "";        //Этотм массив "кастыль"
+                    }
+                }
+                 MixQuestion();                        //Перемешиваем массив с вопросами
+            }
+            catch                       //Отлавливаем экзепшен , если не найден файл с вопросами/ответами
+            {
+                MessageBox.Show("Не найден файл с тестом!");
+            }
+        }
+        private void ShowQuiz(int nr)                              //Метод , отображающий вопросы и ответы
+        {
+            if (nr < 0 | nr > total) return;        //Если счетчик меньше 0 или больше , чем количество вопросов - отмена
+            label_question1.Text = Questions[nr];   //Задаем в label текст вопроса
+            MixAnswer(nr);                          //Перемешиваем ответы
+            if (radioButtonsState[nr, 0] != null)   //Если в массиве с radiobutton , есть значения на текущий вопрос
+                     ShowLastState();               //Отобразить состояния тех radiobutton
+            else                                    //Иначе состояния всех переключателей 
+            {                                       //Перевести в false
+             radioButton_answer1.Checked = false;
+             radioButton_answer2.Checked = false;
+             radioButton_answer3.Checked = false;
+            }
+        }
+        private void Form1_Shown(object sender, EventArgs e)       //При загрузке формы
+        {
+            ShowQuiz(current_nr);                                  //Отобразить первый вопрос
+        }
+        private void button_last_Click(object sender, EventArgs e) //При нажатии кнопкни
+        {
+            if (flag)                                              //Если флаг для прохождения , то 
+            {                                                           
+                if (current_nr > 0)                                //Проверка счетчика, если вопрос не последний , то       
+                {                                                   
+                    CheckAnswer();                                 //Проверяем текущий ответ
+                    if (score > 0) score--;                        //Если количество очков больше 0 , то отнимаем
+                    current_nr--;                                  //Отнимаем 1 у счетчика
+                    ShowQuiz(current_nr);                          //Отображаем вопрос от нового значения счетчика
+                    ShowLastState();                               //Отображаем состояния radiobutton по текущему счетчику
+                }
+            }
+            else
+            {
+                label_result.Text = "";                            //Если флаг равен false
+                button_last.Text = "Previous";                     //Меняем надпись кнопки
+                LoadQuiz();                                        //Загружаем по новой список вопросов , конечно можно было
+                ShowQuiz(current_nr);                              //бы использовать старый массив , но ради интереса хочется
+                                                                   //чтобы вопросы снова перемешались
+            }
+        }
+        private void ShowLastState()                               //Метод отображения состояний radibutton по счетчику
+        {
+            radioButton_answer1.Checked = radioButtonsState[current_nr, 0];
+            radioButton_answer2.Checked = radioButtonsState[current_nr, 1];
+            radioButton_answer3.Checked = radioButtonsState[current_nr, 2];   
+        }
+        private void MixAnswer(int nr)                              //Перемешка ответов
+        {
+            if (ArrayWithMixedAnswer[nr, 0] == "")                  //Если в массиве нет записей о прошлом состоянии поля
+            {                                                       //Text в radiobutton
+                int[] array = new int[3];                           //Создаем массив с номерами , через который будем мешать
+                for (byte b = 0; b < array.Length; b++)             //ответы на вопросы
+                {                                                   
+                label1: array[b] = rnd.Next(0, 3);                  //Если элемент повторился  , то оказываемся здесь 
+                    for (byte d = 0; d < b; d++)                    //Цикл для проверки на повторяемость 
+                    {
+                        if (array[b] == array[d]) goto label1;      //При обнаржуении повтора , переходим по метке для 
+                    }                                               //новой инициализации рандомного значения
+                }                                                   //После того , как массив инициализировался разными не
+                radioButton_answer1.Text = Answers[nr, array[0]];   //повторяющимися значениями , мы присваиваем radiobutton
+                radioButton_answer2.Text = Answers[nr, array[1]];   //разные значения массива с ответами с помощью массива  
+                radioButton_answer3.Text = Answers[nr, array[2]];   //с разными значениями , которые используем ,как индексы
+                correct_number[nr] = Array.IndexOf(array, 0);           //Записываем индекс правильного ответа в переменную
+            }
+            else                                                    //Если  же у нас в массиве записей уже есть состояния полей
+            {                                                       //То для отображения прошлых значений , отображаем значения
+                radioButton_answer1.Text = ArrayWithMixedAnswer[nr,0];  //Этого массива
+                radioButton_answer2.Text = ArrayWithMixedAnswer[nr,1];  //СИТУАЦИЯ ДЛЯ РАЗМЫШЛЕНИЯ: Допустим , что пользователь
+                radioButton_answer3.Text = ArrayWithMixedAnswer[nr,2];  //впервые отвечает на вопрос(ответил неправильно)
+                                                    //переключился на новый вопрос , ответил и на него
+                                                    //вспомнил , что ответ на один вопрос назад , неправильный
+                                                    //переменная correctnumber уже изменила свое значение 
+                                                    //как ему снова ответить на вопрос , но уже чтобы ответ засчитался , как
+                                                    //правильный? 
+            //ИЗМЕНЕНИЯ: изменил поле current_answer на массив , для записей всех правильных ответов
+            }
+        }
+        private void MixQuestion()                                   //Метод , для перемешивания вопросов
+        {                                                            //Размер метода определяется количеством вопросов
+            int[] array = new int[total];                            //Мы создаем здесь копии массива вопросов и ответом
+            string[] CopyQuestion = new string[Questions.Length];    //Чтобы в дальнейшем в уже рабочем массивах тусовать
+            string[,] CopyAnswers = new string[Answers.GetLength(0), //позции из копий
+                                              Answers.GetLength(1)];
+            for (int i = 0; i < total; i++)                          //Заполнение массивов копий
+            {                                                                               
+                CopyQuestion[i] = Questions[i];
+                for (int j = 0; j < Answers.GetLength(1); j++)
+                    CopyAnswers[i, j] = Answers[i, j];
+            }
+            for (byte b = 0; b < array.Length; b++)                  //Абсолютно  алгоритм для тусовки , но уже вопросов и  
+            {                                                        //соответствующих вопросов
+            label2: array[b] = rnd.Next(0, total);
+                for (byte d = 0; d < b; d++)
+                    if (array[b] == array[d]) goto label2;
+            }
+            for(int i = 0; i < array.Length; i++)                    //Тусовка и присвоение значений
+            {
+                Questions[i] = CopyQuestion[array[i]];
+                for(int k = 0; k < CopyAnswers.GetLength(1); k++)
+                    Answers[i, k] = CopyAnswers[array[i], k];
+            }
+        }
+        private void CheckAnswer()
+        {
+            switch (correct_number[current_nr])         //Проверяем ответ в зависимости от текущего счетчика
+            {
+                case 0:
+                    if (radioButton_answer1.Checked)
+                        score++;
+                    break;
+                case 1:
+                    if (radioButton_answer2.Checked)
+                        score++;
+                    break;
+                case 2:
+                    if (radioButton_answer3.Checked)
+                        score++;
+                    break;
+            }
+        }                               //Метод для проверки правильности ТЕКУЩЕГО ответа на вопрос
+        private void Result()                                       //Метод , отображающий результат прохождения
+        {
+            flag = false;                                           //Ставим флаг на false 
+            double result = (score *100) / total;                   //Нахождение процентого составляющего
+            label_result.Text = "Результат " + result + "%";        //Отображение
+            button_last.Text = "Try again?";       
+        }                                    
+        private void radioButton_answer1_MouseClick(object sender, MouseEventArgs e) //События для ответа на вопрос
+        {
+            CheckAnswer();                                                           //Проверка текущего ответа
+            if (current_nr + 1 < total)                                              //Если счетчик меньше максимального 
+            {                                                                        //положения , то состояния radiobutton
+                radioButtonsState[current_nr, 0] = radioButton_answer1.Checked;      //записываются в массив состояний
+                radioButtonsState[current_nr, 1] = radioButton_answer2.Checked;      //Текущие значения полей записываются в    
+                radioButtonsState[current_nr, 2] = radioButton_answer3.Checked;      //"костыльный" массив
+                ArrayWithMixedAnswer[current_nr, 0] = radioButton_answer1.Text;
+                ArrayWithMixedAnswer[current_nr, 1] = radioButton_answer2.Text;
+                ArrayWithMixedAnswer[current_nr, 2] = radioButton_answer3.Text;
+                current_nr++;                                                       //Значения счетчика увеличивается
+                ShowQuiz(current_nr);                                               //Отображение вопросов следующего
+            }                                                                       //положения счетчика
+            else Result();                                                          //Иначе вызвать результат     
+        }
+    }
+}
